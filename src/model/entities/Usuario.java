bc9f437cb81cf32e4bb964c86eea9887c804ab1d
William Ramos <contato.williamramos@outlook.com>
2021-03-17 02:26:05 -0300

    Criação da entidade Usuário e inclusão da lib MySQLConnector (JDBC)

---
+++ src/model/entities/Usuario.java
@@ -0,0 +1,80 @@
+package model.entities;
+
+public class Usuario {
+	
+	private Integer id;
+	private String nome, senha, perfil;
+
+	public Usuario() {
+	}
+	
+	public Usuario(Integer id, String nome, String senha, String perfil) {
+		this.id = id;
+		this.nome = nome;
+		this.senha = senha;
+		this.perfil = perfil;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", senha=" + senha + ", perfil=" + perfil + "]";
+	}
+	
+}
